b0019d2634c945e73a979cec1feddfcc44543f23
skaroweb <52270169+skaroweb@users.noreply.github.com>
2023-09-26 11:42:53 +0530

    first commit

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const axios = require("axios");
 const cors = require("cors");
 const cheerio = require("cheerio");
 const cron = require("node-cron");
-const path = require("path");
-const fs = require("fs"); // For file operations
 const FormData = require("form-data");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 
@@ -81,7 +81,6 @@ app.get("/api/products", async (req, res) => {
       const productData = extractedData[i];
       await postDataToAPIB(productData);
     }
-    //console.log(extractedData);
 
     return res.status(200).json({
       result: crypto,
@@ -98,24 +97,22 @@ app.get("/api/products", async (req, res) => {
 // Post data to another API
 async function postDataToAPIB(productData) {
   const apiBUrl = "https://hot-deals-bazaar-strapi.onrender.com/api/products"; // Replace with the actual API B URL
-  //console.log(productData);
+
   try {
     /******************** check products already Exists  *************/
 
-    const existingDataResponse = await axios.get(apiBUrl); // Assuming the API supports GET requests for existing data
+    const existingDataResponse = await axios.get(apiBUrl);
 
     const existingData = existingDataResponse.data.data;
-    // console.log(existingData);
-    // Clean and format a string for comparison
+
     function cleanAndFormatString(str) {
       return str
-        .trim() // Remove leading and trailing spaces
-        .replace(/[^\w\s]/g, "") // Remove non-alphanumeric characters
-        .replace(/\s+/g, " ") // Remove consecutive spaces
-        .toLowerCase(); // Convert to lowercase for case-insensitive comparison
+        .trim()
+        .replace(/[^\w\s]/g, "")
+        .replace(/\s+/g, " ")
+        .toLowerCase();
     }
 
-    // // Assuming there's a property in your productData that can be used to uniquely identify the product (e.g., productLink)
     const isDataAlreadyExists = existingData.some(
       (item) =>
         cleanAndFormatString(item.attributes.title) ===
@@ -128,112 +125,69 @@ async function postDataToAPIB(productData) {
     }
     /******************** check products already Exists  *************/
 
-    /******************** Image stored in Local  *************/
+    /******************** Download Image  *************/
+    const imageFileName = path.basename(productData.productImgLink);
+    const imagePath = path.join(__dirname, "uploads", imageFileName);
 
-    // Assuming 'jsonData' holds the JSON data with 'productImgLink'
-
-    const productImgLink = productData.productImgLink;
-    const response = await axios.get(productImgLink, {
-      responseType: "arraybuffer",
+    const imageResponse = await axios.get(productData.productImgLink, {
+      responseType: "stream",
     });
 
-    // Extract the file extension from the URL
-    const ext = path.extname(productImgLink).split("?")[0]; // Remove query parameters
+    const writeStream = fs.createWriteStream(imagePath);
+    imageResponse.data.pipe(writeStream);
 
-    const productTitle = productData.title;
-    const cleanedProductTitle = productTitle
-      .replace(/-/g, " ") // Replace dashes with spaces
-      .replace(/[^\w\s]/g, "") // Remove non-alphanumeric characters
-      .replace(/\s+/g, " ") // Remove consecutive spaces
-      .trim(); // Remove leading and trailing spaces
+    await new Promise((resolve, reject) => {
+      writeStream.on("finish", resolve);
+      writeStream.on("error", reject);
+    });
 
-    // Create a cleaned-up file name without special characters
-    const fileName = `${cleanedProductTitle}.${ext.replace(/\W/g, "")}`;
-
-    // Specify the full image path including the uploads directory
-    const imagePath = path.join(
-      "https://hot-deals-bazaar-express.onrender.com",
-      "uploads",
-      fileName
-    );
-
-    // Create necessary directories if they don't exist
-    const dirPath = path.dirname(imagePath);
-    if (!fs.existsSync(dirPath)) {
-      fs.mkdirSync(dirPath, { recursive: true });
-    }
-
-    // Write the image data to the file
-    fs.writeFileSync(imagePath, response.data);
-
-    //console.log(response.data);
-
-    /******************** Image stored in Local  *************/
+    /******************** Download Image  ***************/
 
     /******************** Image Upload  *************/
-    const filenameWithExt = path.basename(imagePath);
-    const uploadpathwithFilename = path.relative(
-      "https://hot-deals-bazaar-express.onrender.com/",
-      imagePath
-    );
+    const formData = new FormData();
+    formData.append("files", fs.createReadStream(imagePath), {
+      filename: imageFileName,
+    });
+    formData.append("ref", "api::product.product");
+    formData.append("field", "productimage");
 
-    const responses = await axios.post(apiBUrl, {
-      data: {
-        //  productImgLink: imagePath,
-        productLink: productData.productLink,
-        title: productData.title,
-        dealPrice: productData.dealPrice,
-        originalPrice: productData.originalPrice,
-        platform: productData.platform,
-        discount: productData.discount,
-        // publishedAt: null,
+    const strapiApiUrl = "https://hot-deals-bazaar-strapi.onrender.com";
+    const uploadUrl = `${strapiApiUrl}/api/upload`;
+
+    const uploadResponse = await axios.post(uploadUrl, formData, {
+      headers: {
+        ...formData.getHeaders(),
       },
     });
 
-    console.log("Data posted to API B:", responses.data);
-
-    // Create a new FormData instance
-    const formData = new FormData();
-
-    // Append the actual file to the FormData object
-    const filePath = uploadpathwithFilename;
-    const fileStream = fs.createReadStream(filePath);
-    formData.append("files", fileStream, {
-      filename: filenameWithExt,
-    });
-    formData.append("ref", "api::product.product");
-    formData.append("refId", responses.data.data.id);
-    formData.append("field", "productimage");
-    // Replace with your Strapi API URL
-    const strapiApiUrl = "https://hot-deals-bazaar-strapi.onrender.com/";
-    const uploadUrl = `${strapiApiUrl}/api/upload`;
-
-    axios
-      .post(uploadUrl, formData, {
-        headers: {
-          ...formData.getHeaders(),
-        },
-      })
-      .then((response) => {
-        //  console.log(response.data);
-      })
-      .catch((error) => {
-        console.error("Error uploading image:", error.message);
-      });
+    console.log("Image uploaded successfully to Strapi.");
 
     /******************** Image Upload  ***************/
 
-    /******************** New Products added  ***************/
+    /******************** Data Posting  *************/
+    const postData = {
+      productLink: productData.productLink,
+      title: productData.title,
+      dealPrice: productData.dealPrice,
+      originalPrice: productData.originalPrice,
+      platform: productData.platform,
+      discount: productData.discount,
+      productimage: uploadResponse.data[0].id, // Use the uploaded image ID
+    };
+
+    // Post the data to your API B
+    await axios.post(apiBUrl, postData);
+    console.log("Data posted to API B.");
+
+    /******************** Data Posting  ***************/
   } catch (error) {
     console.error("Error posting data to API B:", error.message);
   }
 }
 
-/******************** New Products added  ***************/
+/****************** Schedule the cron job  ***************/
 
-/******************** Schedule the cron job  ***************/
-
-//Schedule the cron job to fetch and post data every 5 minutes
+// Schedule the cron job to fetch and post data every 5 minutes
 
 // cron.schedule("*/1 * * * *", async () => {
 //   try {
@@ -251,7 +205,7 @@ async function postDataToAPIB(productData) {
 //   }
 // });
 
-/******************** Schedule the cron job  ***************/
+/****************** Schedule the cron job  ***************/
 
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
